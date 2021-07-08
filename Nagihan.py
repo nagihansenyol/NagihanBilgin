d6c0add7b40be4b136633c18bc043223394e3321
nagihansenyol <nagihansenyol@yahoo.com>
2021-07-08 13:26:57 +0000

    Dosyada degisiklik yapildi

--- Nagihan.py
+++ Nagihan.py
@@ -1,7 +1,8 @@
 import json
-dosya=open("myfile.json","r")
+dosya=open("Nagihan.json","r")
 json_dosya=json.load(dosya)
 print("ADINIZ:",json_dosya["ad"])
 print("SOYADINIZ:",json_dosya["soyad"])
 print("API_KEY:",json_dosya["access_token"])
-
+print("YASADIGINIZ SEHIR:",json_dosya["sehir"])
+print("YASADIGINIZ ULKE:",json_dosya["ulke"])
