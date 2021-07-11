a56214f75a78b164aff3bc48fce37f686d779a3e
nagihansenyol <nagihansenyol@yahoo.com>
2021-07-11 01:17:32 +0000

    Python dosyasinda degisiklik yapildi

--- Nagihan.py
+++ Nagihan.py
@@ -3,4 +3,4 @@ dosya=open("Nagihan.json","r")
 json_dosya=json.load(dosya)
 print("ADINIZ:",json_dosya["kimlik"]["ad"])
 print("SOYADINIZ:",json_dosya["kimlik"]["soyad"])
-
+print("KIMLIGINIZ:",json_dosya["kimlik"])
