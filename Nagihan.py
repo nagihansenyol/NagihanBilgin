43dcf6b59ed7abd6177edc393731581d98d4dae8
nagihansenyol <nagihansenyol@yahoo.com>
2021-07-11 01:02:13 +0000

    Ilk commit

--- Nagihan.py
+++ Nagihan.py
@@ -1,8 +1,6 @@
 import json
 dosya=open("Nagihan.json","r")
 json_dosya=json.load(dosya)
-print("ADINIZ:",json_dosya["ad"])
-print("SOYADINIZ:",json_dosya["soyad"])
-print("API_KEY:",json_dosya["access_token"])
-print("YASADIGINIZ SEHIR:",json_dosya["sehir"])
-print("YASADIGINIZ ULKE:",json_dosya["ulke"])
+print("ADINIZ:",json_dosya["kimlik"]["ad"])
+print("SOYADINIZ:",json_dosya["kimlik"]["soyad"])
+
